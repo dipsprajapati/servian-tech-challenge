473369d273def8567b601a61dcf57ba75d4b5f93
dipsprajapati <dips.p.007@gmail.com>
2020-10-10 22:53:26 +0800

    terraform files added

---
+++ iac_gke_cluster/variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  default = "servian-tech-challenge"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "cluster" {
+  default = "servian-app-v2"
+}
+
+variable "credentials" {
+  default = "/Users/dipesh/mydocker/servian-tech-challenge-527f3d89ef90.json"
+}
+
+variable "kubernetes_min_ver" {
+  default = "latest"
+}
+
+variable "kubernetes_max_ver" {
+  default = "latest"
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
+
+variable "app_name" {
+  default = "servian-app-v2"
+}
